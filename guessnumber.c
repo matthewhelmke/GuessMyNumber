6b66b0b178cf56661e65eda5d840bf867ed047d2
Matthew Helmke <matthew@matthewhelmke.com>
2020-12-29 16:03:37 -0600

    clean up more

--- guessnumber.c
+++ guessnumber.c
@@ -1,6 +1,6 @@
 /* Guess my number game
 *
-* --A Python version of a silly game I made on my programmable
+* --A Clang version of a silly game I made on my programmable
 *   calculator when I was bored in math class in 1987, with a couple of
 *   additions like input validation and computer guesses.
 *
