83f65d4559621eeb299e1d8033794504a52ec0a4
Matthew Helmke <matthew@matthewhelmke.com>
2021-02-14 08:45:43 -0600

    add taunts and tracking lowmax and highmax to limit computerguesses

--- guessnumber.rkt
+++ guessnumber.rkt
@@ -27,7 +27,7 @@
 ; Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 ; Print a description of the game, with rules, to the screen
-(displayln "Welcome to Guess My Number!
+(printf "Welcome to Guess My Number!
 
 The computer will select a random whole number between 1 and 100.
 Your goal is to guess that number. You will get a turn, then a computer
@@ -35,25 +35,38 @@ player will get a turn. Each of you are aware of the other's guesses.
 The first one to guess the number correctly will win. Try to guess in
 as few turns as possible.
 
-Here we go!")
+Here we go!\n\n")
 
+; Define computerguess initial guess
+(define computerguess (random 1 101))
+(define highmax 100)
+
+(define lowmax 1)
 ; Define the guessing function, with validation rules
 (define (inquire-user secretnumber)
-  (display "What is your guess? ")
+  (printf "What is your guess? ")
   (define userguess (string->number (read-line)))
   ; Validate userguess against multiple criteria
-  (cond [(not (integer? userguess)) (displayln "Only whole numbers from 1 to 100 are allowed. Please try again. ") (inquire-user secretnumber)]
-        [(< userguess 1) (displayln "Only whole numbers from 1 to 100 are allowed.  Your guess is out of range. Please try again. ") (inquire-user secretnumber)]
-        [(> userguess 100) (displayln "Only whole numbers from 1 to 100 are allowed.  Your guess is out of range. Please try again. ") (inquire-user secretnumber)]
-        [(> secretnumber userguess) (displayln "Your guess is too low.")]
-        [(< secretnumber userguess) (displayln "Your guess is too high.")]
-        [(= secretnumber userguess) (displayln "Your guess is correct! Congratulations!") (exit)])
+  (cond [(not (integer? userguess)) (printf "Only whole numbers from 1 to 100 are allowed. Please try again. ") (inquire-user secretnumber)]
+        [(< userguess 1) (printf "Only whole numbers from 1 to 100 are allowed.  Your guess is out of range. Please try again. ") (inquire-user secretnumber)]
+        [(> userguess 100) (printf "Only whole numbers from 1 to 100 are allowed.  Your guess is out of range. Please try again. ") (inquire-user secretnumber)]
+        [(< userguess lowmax) (printf "That guess was lower than a previous guess that was too low. Pay attention!\n")]
+        [(> userguess highmax) (printf "Wake up! That guess was higher than an earlier guess that was too high.\n")]
+        [(> secretnumber userguess) (printf "Your guess is too low.\n") (set! lowmax userguess)]
+        [(< secretnumber userguess) (printf "Your guess is too high.\n") (set! highmax userguess)]
+        [(= secretnumber userguess) (printf "\n*********************************************\nYour guess is correct! Congratulations!\n*********************************************\n") (exit)])
   ; Computer gets a turn
-  (define computerguess (random 1 100))
-    (cond [(> secretnumber computerguess) (displayln "The computer guess is too low.")]
-        [(< secretnumber computerguess) (displayln "The computer guess is too high.")]
-        [(= secretnumber computerguess) (displayln "The computer guess is correct!") (exit)])
+  (printf "highmax = ~a. \n" highmax) ;testing
+  (printf "lowmax = ~a. \n" lowmax) ;testing
+  (set! computerguess (+ (random (- highmax lowmax)) lowmax))
+  (cond [(> secretnumber computerguess) (printf "The computer guessed ~a and that is too low.\n" computerguess) (set! lowmax computerguess)]
+        [(< secretnumber computerguess) (printf "The computer guessed ~a and that is too high.\n" computerguess) (set! highmax computerguess)]
+        [(= secretnumber computerguess) (printf "\n*********************************************\nThe computer guess of ~a is correct!\n**********************************************\n" computerguess) (exit)])
+  (printf "highmax = ~a. \n" highmax) ;testing
+  (printf "lowmax = ~a. \n" lowmax) ;testing
+  (printf "secretnumber = ~a. \n" secretnumber) ;testing
+ 
   (inquire-user secretnumber))
 
 ; Call the function with "secretnumber" defined as a random number between 1 and 100
-(inquire-user (random 1 100))
+(inquire-user (random 1 101))
