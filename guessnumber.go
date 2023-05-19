f1dcfdb638bfc37dba73864d78494b7db3ef52d7
Matthew Helmke <matthew@matthewhelmke.com>
2023-05-18 19:05:41 -0500

    add comments to golang - WIP

--- guessnumber.go
+++ guessnumber.go
@@ -38,31 +38,43 @@ func main() {
 
     minNumber := 1
     maxNumber := 100
+    // Get a random number
     secretNumber := rand.Intn(maxNumber-minNumber+1) + minNumber
 
+    // Print a description of the game, with rules, to the screen
     fmt.Println("Welcome to Guess My Number!\n\nThe computer will select a random whole number between 1 and 100.\n\nYour goal is to guess that number. You will get a turn, then a computer player will get a turn. Each of you are aware of the other's guesses. The first one to guess the number correctly will win. Try to guess in as few turns as possible.\n\nHere we go!\n\n")
     fmt.Printf("I'm thinking of a number between %d and %d. Can you guess it?\n", minNumber, maxNumber)
 
     var guess int
-    numGuesses := 0
+    totalGuesses := 0
 
+    // the main bit
     for {
         fmt.Print("What is your guess? ")
+        // let the user input any number they want, then 
+        // TODO: check if an integer between 1-100 - current check does not work
         _, err := fmt.Scanf("%d", &guess)
         if err != nil {
         fmt.Println("Invalid! Please enter a whole number between 1 and 100: ")
         continue
         }
 
-        numGuesses++
-
+        totalGuesses++
+        
+        // TODO: some taunts for silly errors in user guesses
+        
+        
+        // evaluate the guess
         if guess < secretNumber {
             fmt.Println("Your guess is too low.\n")
         } else if guess > secretNumber {
             fmt.Println("Your guess is too high.\n")
         } else {
-            fmt.Printf("\n*********************************************\n   Your guess is correct! Congratulations!\n   It took %d total guesses.\n*********************************************\n\n", numGuesses)
+            fmt.Printf("\n*********************************************\n   Your guess is correct! Congratulations!\n   It took %d total guesses.\n*********************************************\n\n", totalGuesses)
         break
         }
+        
+        //TODO: computer does a random guess, within the range of current reasonable values
+        //TODO: evaluate computer guess and pring it's guess and whether it was high/low
     }
 }
